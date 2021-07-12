b30b9429b31d5bedfd9d7c26f2c7fb04e128ad48
d-serj <serj.dub1na@gmail.com>
2021-07-12 22:43:41 +0300

    Update comhdlc

--- src/comhdlc.h
+++ src/comhdlc.h
@@ -8,41 +8,37 @@
 
 #include <tinyframe/TinyFrame.h>
 
+enum eComHdlcFrameTypes
+{
+    eComhdlcFrameType_ACK  = 1,
+    eComhdlcFrameType_REQ  = 2,
+    eComhdlcFrameType_DATA = 3,
+    eComhdlcFrameType_ERR  = 4,
+};
+
+enum eComHdlcCommands
+{
+    eCmdWriteFile            = 1,
+    eCmdWriteFileFinish      = 2,
+    eComHdlcAnswer_HandShake = 3,
+};
+
 class comhdlc : public QObject
 {
     Q_OBJECT
 public:
     comhdlc(QString comName);
     ~comhdlc();
-    void send_data(const QByteArray &data);
     void transfer_file(const QByteArray &file, QString file_name);
     bool is_comport_connected(void) const;
 
 private:
-
-    enum eComHdlcFrameTypes
-    {
-        eComhdlcFrameType_ACK  = 1,
-        eComhdlcFrameType_REQ  = 2,
-        eComhdlcFrameType_DATA = 3,
-        eComhdlcFrameType_ERR  = 4,
-    };
-
-    enum eComHdlcCommands
-    {
-        eCmdWriteFile            = 1,
-        eCmdWriteFileFinish      = 2,
-        eComHdlcAnswer_HandShake = 3,
-    };
-
-
     QString com_port_name;
-    QTimer *timer = nullptr;
+    QTimer *timer    = nullptr;
+    QTimer *timer_tf = nullptr;
     QByteArray file_send;
     QByteArray send_buffer;
     QByteArrayList file_chunks;
-    quint32 file_chunk_current;
-
 
     static TinyFrame *tf;
     static quint64 bytes_written;
@@ -51,10 +47,9 @@ private:
     static void send_byte(uint8_t data);
     static void process_buffer(const uint8_t* buff, uint16_t buff_len);
     void send_handshake(void);
+    void tf_handle_tick(void);
     void file_send_routine(void);
     void send_command(uint8_t command);
-
-    /** Callbacks for TinyFrame */
 
 private slots:
     void comport_data_available();
